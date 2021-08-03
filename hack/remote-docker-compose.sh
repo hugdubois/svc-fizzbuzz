57149616cd535266b0fecae21e2fed3f6538d0e7
Hugues Dubois <hugdubois@gmail.com>
2021-08-03 15:13:07 +0200

    chore: add hack directory with some helpers script

---
+++ hack/remote-docker-compose.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+BRANCH=develop
+BASE_URL=https://raw.githubusercontent.com/hugdubois/svc-fizzbuzz/$BRANCH
+SVC_TAG=0.0.5_dev
+
+mkdir -p svc-fizzbuzz-compose
+cd svc-fizzbuzz-compose
+mkdir -p infra
+cd infra
+curl -O $BASE_URL/infra/config.monitoring
+mkdir -p prometheus
+cd prometheus
+curl -O $BASE_URL/infra/prometheus/prometheus.yml
+cd ../..
+curl -O $BASE_URL/docker-compose.yml
+
+echo "TAG=$SVC_TAG" > .env
+docker-compose -d up
